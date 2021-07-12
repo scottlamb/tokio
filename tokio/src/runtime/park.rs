e759fbb3297f16b40791145913df2e9010cdbee1
Scott Lamb <slamb@slamb.org>
2021-07-11 20:56:26 -0700

    do less spin looping

--- tokio/src/runtime/park.rs
+++ tokio/src/runtime/park.rs
@@ -119,18 +119,14 @@ impl Unpark for Unparker {
 impl Inner {
     /// Parks the current thread for at most `dur`.
     fn park(&self) {
-        for _ in 0..3 {
-            // If we were previously notified then we consume this notification and
-            // return quickly.
-            if self
-                .state
-                .compare_exchange(NOTIFIED, EMPTY, SeqCst, SeqCst)
-                .is_ok()
-            {
-                return;
-            }
-
-            thread::yield_now();
+        // If we were previously notified then we consume this notification and
+        // return quickly.
+        if self
+            .state
+            .compare_exchange(NOTIFIED, EMPTY, SeqCst, SeqCst)
+            .is_ok()
+        {
+            return;
         }
 
         if let Some(mut driver) = self.shared.driver.try_lock() {
